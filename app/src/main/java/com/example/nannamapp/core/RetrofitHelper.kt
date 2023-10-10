25ea7ab17862eaa715a9019c2ca0a48d0f12b327
EDUART USSIEL DIRCIO CAYON <zS19030173@estudiantes.uv.mx>
2023-10-10 13:23:27 -0600

    Init

--- app/src/main/java/com/example/nannamapp/core/RetrofitHelper.kt
+++ app/src/main/java/com/example/nannamapp/core/RetrofitHelper.kt
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitHelper {
     fun getRetrofit(): Retrofit {
         return Retrofit.Builder()
-            .baseUrl("")
+            .baseUrl("http://10.0.2.2:5093")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
