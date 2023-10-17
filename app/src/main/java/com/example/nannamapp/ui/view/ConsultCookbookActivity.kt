728fb6c15f46f35c63b5ec955e0492e3b99599aa
EDUART USSIEL DIRCIO CAYON <zS19030173@estudiantes.uv.mx>
2023-10-17 13:02:59 -0600

    Configuracion de recycler

--- app/src/main/java/com/example/nannamapp/ui/view/ConsultCookbookActivity.kt
+++ app/src/main/java/com/example/nannamapp/ui/view/ConsultCookbookActivity.kt
@@ -1,18 +1,16 @@
 package com.example.nannamapp.ui.view
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R
 import android.os.Bundle
-import android.widget.ArrayAdapter
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.namnam.data.model.CategoryProvider
-import com.example.namnam.data.model.Recipe
-import com.example.nannamapp.R
-import com.example.nannamapp.data.model.RecipeProvider
+
 import com.example.nannamapp.databinding.ActivityConsultCookbookBinding
 import com.example.nannamapp.ui.viewModel.CookBookViewModel
+
 
 class ConsultCookbookActivity : AppCompatActivity() {
     private lateinit var binding: ActivityConsultCookbookBinding
@@ -27,19 +25,20 @@ class ConsultCookbookActivity : AppCompatActivity() {
         cookbookViewModel.onCreate()
 
         // Configura el RecyclerView
-        binding.recyclerview.layoutManager = LinearLayoutManager(this)
-
+        //binding.recyclerview.layoutManager = LinearLayoutManager(this)
+        val recyclerView = findViewById<RecyclerView>(com.example.nannamapp.R.id.recyclerview)
+        val layoutManager = GridLayoutManager(this, 2)
+        recyclerView.layoutManager = layoutManager
         // Inicializa el adaptador
         recipeAdapter = CookBookAdapter()
+        binding.recyclerview.adapter = recipeAdapter
 
-        cookbookViewModel.cookBookModel.observe(this, Observer {
-            recipeList = mutableListOf<Recipe>()
-            for(i in 0..RecipeProvider.cookBook.size-1){
-                recipeList.add(RecipeProvider.cookBook.get(i))
+        // Observa los cambios en la lista de recetas
+        cookbookViewModel.cookBookModel.observe(this, Observer { recipes ->
+            recipes?.let {
+                recipeAdapter.setData(it)
             }
-
         })
     }
-
 
 }
